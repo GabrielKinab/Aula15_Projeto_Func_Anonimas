0ed08022bc84aaf3c20b961170ccfaf6f81bc60c
GabrielKinab <gabriel.kinab.matos@escola.pr.gov.br>
2024-10-07 09:42:14 -0300

    subindo arquivo

---
+++ script.js
@@ -0,0 +1,22 @@
+//FUNÇÕES ANÔNIMAS
+//sintaxe : ()=>()
+/*
+() - padrão de passagem de argumento em funções
+=>  : é o escopo (bloco de código) da função propriamente*/
+
+
+function somar(a,b){
+    console.log(total);
+    let.total = a+b;
+}
+somar(2,10);
+//atribuindo uma funçao anônima a uma vaiavel
+let subtrair = (num1, num2)=>{
+    let total = num1-num2;
+    console.log(total);
+}
+subtrair(15,17);
+let numeros = [1,2,3,10];
+numeros.map((item)=>{
+console.log((item));
+});
